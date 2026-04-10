a04a31130c608fa5288625e2088f35b492e58e12
Colin Gray <colin.gray@equalexperts.com>
2026-04-10 18:11:36 +0100

    Removed commented out code

--- src/config/nunjucks/context/build-navigation.js
+++ src/config/nunjucks/context/build-navigation.js
@@ -1,19 +1,9 @@
 export function buildNavigation(request) {
   return [
-    // {
-    //   text: 'Home',
-    //   href: '/',
-    //   current: request?.path === '/'
-    // },
     {
       text: 'Projects',
       href: '/projects',
       current: request?.path?.startsWith('/projects')
     }
-    // {
-    //   text: 'About',
-    //   href: '/about',
-    //   current: request?.path === '/about'
-    // }
   ]
 }
