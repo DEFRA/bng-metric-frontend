5433ce9e143d513f6dfcc944c89cd82bd1590c4a
jbfarrar <johny.farrar@esynergy.co.uk>
2026-05-27 14:37:35 +0100

    wip: sortable table html

--- src/server/habitat-list/controller.js
+++ src/server/habitat-list/controller.js
@@ -1,7 +1,8 @@
 import { fetchProject } from '../common/services/projects.js'
 import {
   formatTotalAreaSize,
-  formatTotalLengthSize
+  formatTotalLengthSize,
+  formatAreaHectares
 } from '../common/helpers/format-habitat-values.js'
 
 export const getController = {
@@ -21,12 +22,36 @@ export const getController = {
       )
     }
 
+    const habitats = project?.project?.baseline?.habitats ?? null
+
+    let habitatRows = null
+
+    if (habitats) {
+      habitatRows = habitats.map((h) => [
+        {
+          html: `<a class="govuk-link" href="/baseline-habitat-details/${h.featureId}">${h.ref}</a>`
+        },
+        { text: h.type ?? '' },
+        {
+          text: formatAreaHectares(h.sizeSquareMetres),
+          attributes: {
+            'data-sort-value': h.sizeSquareMetres
+          }
+        },
+        { text: h.distinctiveness ?? '' },
+        { text: h.condition ?? '' },
+        { text: h.units },
+        { text: h.status ?? 'Complete' }
+      ])
+    }
+
     return h.view('habitat-list/habitat-list', {
       pageTitle: 'On-site baseline habitats',
       heading: 'On-site baseline habitats',
       caption: projectName,
       projectId: id,
-      totalSizes
+      totalSizes,
+      habitatRows
     })
   }
 }
